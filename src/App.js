55ec973cb66861d49a9c80c033368868a2de26e3
TaylorDarneille <tmdarneille@gmail.com>
2022-02-01 13:41:47 -0800

    data circulates correctly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const roster = [
+  'Taylor',
+  'April',
+  'Weston',
+  'Jason'
+]
 
 function App() {
+  const [injured, setInjured] = useState([])
+  // const [court, setCourt] = useState(roster)
+  // const [deck, setDeck] = useState('')
+  // const [hotSeat, setHotSeat] = useState('')
+  // const [bench, setBench] = useState([])
+  const [court, setCourt] = useState({queue: roster, deck: '', hotSeat: '', bench: []})
+
+  const durstenfeldShuffle = (q) => {
+    let i = q.length - 1
+    while(i>0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [q[i], q[j]] = [q[j], q[i]];
+      i--
+    }
+    return q
+  }
+
+  const play  = () => {
+    // if deck is empty but hotSeat is full, reset court with shuffled queue
+    if(!court.deck && court.queue.length===0) {
+      let shuffledQueue = durstenfeldShuffle(roster)
+      setCourt({queue: shuffledQueue, deck: '', hotSeat: '', bench: []})
+      return
+    }
+    let newCourt = {}
+    // if hotSeat, move hotSeat to bench
+    if(court.hotSeat) newCourt.bench = court.bench.concat([court.hotSeat])
+    else newCourt.bench = court.bench
+    // if onDeck, move onDeck to hotSeat
+    if(court.deck) newCourt.hotSeat = court.deck
+    else newCourt.hotSeat = court.hotSeat // TODO modify this
+    // if queue isn't empty, move one player to deck
+    // new queue is old queue minus the first elem
+    if(court.queue.length>0) {
+      newCourt.deck = court.queue[0]
+      newCourt.queue = court.queue.slice(1)
+    } else {
+      newCourt.deck = ''
+      newCourt.queue = []
+    }
+    setCourt(newCourt)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={play}>Play</button>
     </div>
   );
 }
